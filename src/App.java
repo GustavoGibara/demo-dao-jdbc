1b1c18227817742fd4e19a6e71ec89611a7f4ec4
Gibara <gustavo.gibara@gmail.com>
2026-02-08 16:24:30 -0300

    Seller entity class

--- src/App.java
+++ src/App.java
@@ -1,9 +1,15 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Department obj = new Department(1, "Books");
+        Seller obj2 = new Seller(1, "Gustavo", "gustavo@gmail.com", LocalDate.parse("22/03/2001", DateTimeFormatter.ofPattern("dd/MM/yyyy")), 2000.0, obj);
 
         System.out.println(obj);
+        System.out.println(obj2);
     }
 }
